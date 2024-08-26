5eb520712d43f1ca98127bffd76e25bd7ce9f5a1
Zuzanna Rachuba <zuziarachuba@gmail.com>
2024-08-26 17:35:56 +0200

    initial commit

---
+++ readme.py
@@ -0,0 +1,5 @@
+input = input("This is a place where you can monitor your blood sugar levels everyday. \nThis program will collect your data, and give you your average blood glukose level, and after 3 days - posibble A1C level. \nIf you don't know where to start, write 'help' ")
+if input == "help":
+    print("something")
+else:
+    print("something else")
